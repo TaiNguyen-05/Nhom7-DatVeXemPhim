a8d699272bc41bcca01847f8b690048e49597396
hoàng minh <minhnlh4017@ut.edu.vn>
2025-08-29 15:19:57 +0700

    cập nhật CSS classes cho các trường password

--- booking/forms.py
+++ booking/forms.py
@@ -42,3 +42,21 @@ class UserRegistrationForm(UserCreationForm):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Cập nhật CSS classes cho các trường password
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Nhập mật khẩu'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Xác nhận mật khẩu'
+        })
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        
+        if commit:
+            user.save()
